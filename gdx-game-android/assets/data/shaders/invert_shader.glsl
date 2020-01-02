215091ed98920a241e5f0833ff7c40743c0bd9fc
exformat <exformat1988@gmail.com>
2020-01-02 22:09:09 +0600

    add invert color shader

---
+++ gdx-game-android/assets/data/shaders/invert_shader.glsl
@@ -0,0 +1,16 @@
+#ifdef GL_ES 
+	#define LOWP lowp 
+	precision mediump float; 
+#else 
+	#define LOWP 
+#endif 
+
+varying LOWP
+vec4 v_color; 
+uniform vec2 v_texCoords; 
+uniform sampler2D u_texture; 
+
+void main(){
+	gl_FragColor = v_color * texture2D(u_texture, v_texCoords);
+	gl_FragColor.rgb=1.0-gl_FragColor.rgb;
+}
