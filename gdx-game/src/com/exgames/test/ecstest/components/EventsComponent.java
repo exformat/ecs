de3ae8c097edd03d6298e42f48a3c861572f9b25
exformat <exformat1988@gmail.com>
2020-01-02 22:15:23 +0600

    event component(not work)

--- gdx-game/src/com/exgames/test/ecstest/components/EventsComponent.java
+++ gdx-game/src/com/exgames/test/ecstest/components/EventsComponent.java
@@ -5,7 +5,16 @@ import com.badlogic.gdx.math.Vector2;
 public class EventsComponent implements Component
 {
 	private boolean collision = false;
+	private boolean playSound = false;
 	private Vector2 collisionCoord = new Vector2(0, 0);
+
+	public void setPlaySound(boolean playSound) {
+		this.playSound = playSound;
+	}
+
+	public boolean isPlaySound() {
+		return playSound;
+	}
 
 	public void setCollisionCoord(Vector2 collisionCoord) {
 		this.collisionCoord = collisionCoord;
