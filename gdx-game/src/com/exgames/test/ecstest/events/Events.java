6c7d0647f275a01da3a45fb84ff9f5b56c7e98e0
exformat <exformat1988@gmail.com>
2020-01-02 13:26:07 +0600

    костыль

---
+++ gdx-game/src/com/exgames/test/ecstest/events/Events.java
@@ -0,0 +1,8 @@
+package com.exgames.test.ecstest.events;
+import com.badlogic.gdx.math.Vector2;
+
+public class Events
+{
+	public static boolean isCollision = false;
+	public static Vector2 collisionCoord = new Vector2();
+}
