7acef18fe71c3afd0a3817cf79d0d7caf2528379
exformat <exformat1988@gmail.com>
2020-01-03 13:12:20 +0600

    add tap component

---
+++ gdx-game/src/com/exgames/test/ecstest/components/control_components/TapComponent.java
@@ -0,0 +1,6 @@
+package com.exgames.test.ecstest.components.control_components;
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
+
+public class TapComponent extends Vector2 implements Component {
+}
