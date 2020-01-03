d336203700b234a68ec6f4110c73fa74d801c1fb
exformat <exformat1988@gmail.com>
2020-01-03 20:09:37 +0600

    add tapSystem into engine

--- gdx-game/src/com/exgames/test/ecstest/Engine.java
+++ gdx-game/src/com/exgames/test/ecstest/Engine.java
@@ -7,32 +7,41 @@ import com.exgames.test.ecstest.systems.MoveSystem;
 import com.exgames.test.ecstest.systems.RenderSystem;
 import com.exgames.test.ecstest.systems.ShaderSystem;
 import com.exgames.test.ecstest.systems.SoundSystem;
+import com.exgames.test.ecstest.systems.control_systems.TapSystem;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Engine extends PooledEngine
 {
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
+	private Viewport viewport;
 	private Rectangle screenRect;
 	
 	private RenderSystem renderSystem;
 	private MoveSystem moveSystem;
 	private SoundSystem soundSystem;
 	private ShaderSystem shaderSystem;
+	
+	private TapSystem tapSystem;
 
-	public Engine(SpriteBatch batch, OrthographicCamera camera, Rectangle screenRect) {
+	public Engine(SpriteBatch batch, OrthographicCamera camera, Viewport viewport, Rectangle screenRect) {
 		this.batch = batch;
 		this.camera = camera;
 		this.screenRect = screenRect;
+		this.viewport = viewport;
 		
 		renderSystem = new RenderSystem(batch, camera);
 		moveSystem = new MoveSystem(screenRect);
 		soundSystem = new SoundSystem();
 		shaderSystem = new ShaderSystem(batch, camera);
 		
+		tapSystem = new TapSystem(viewport);
+		
 		addSystem(renderSystem);
 		addSystem(shaderSystem);
 		addSystem(moveSystem);
 		addSystem(soundSystem);
+		addSystem(tapSystem);
 	}
 	
 	public void dispose(){
